7be191255b3e6814ddfe366177dae74828f2c471
Rafael <scheidt.jans@gmail.com>
2021-07-12 23:42:10 -0300

    criando primeira resposta

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const discord = require ('discord.js')  
+const envs = require ('./envs.json')
+const client = new discord.Client()
+
+client.login(envs.API_Key)
+
+client.on('message', (mensagem) => {
+    if (mensagem.content.startsWith(envs.prefix)){
+        mensagem.channel.send('Tô lendo sua mensagem')
+    }
+}
+
+)
